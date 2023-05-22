428e37e2000b5c1a01862de83375487b8eccf52a
junhyuk <slothing96@gmail.com>
2023-05-22 17:49:19 +0900

    feat/firstdata

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=20, blank=True)
+    profile = models.TextField(max_length=100, blank=True)
+    profile_img = models.ImageField(upload_to='profile_pics', blank=True)
