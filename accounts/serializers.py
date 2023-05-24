93184e49da071e68f55fbc5fd22d61ccd1fc34cc
junhyuk <slothing96@gmail.com>
2023-05-24 14:33:20 +0900

    feat/profilepage

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import User
+
+
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    nickname = serializers.CharField(max_length=15, required=True)
-
+    username = serializers.CharField(max_length=20, read_only=True)
+    nickname = serializers.CharField(max_length=20, read_only=True)
+    profile = serializers.CharField(max_length=100, read_only=True)
     class Meta:
         model = User
         fields = '__all__'
